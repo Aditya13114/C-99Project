9f40c624ae3f165b4872c795af9d7669918f405d
Aditya13114 <75022510+Aditya13114@users.noreply.github.com>
2021-07-18 15:30:41 +0530

    Add files via upload

---
+++ organise.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+path = "/Users/adityadeonarayan/Desktop/TrexGame"
+listoffiles = os.listdir(path)
+
+for files in listoffiles:
+    name,ext = os.path.splitext(files)
+    ext = ext[1:]
+    print(ext)
+    if ext ==  "":
+        continue
+    if os.path.exists(path+'/'+ext):
+        shutil.move(path+'/'+files , path+'/'+ext+'/'+files)
+    else :
+        os.makedirs(path+'/'+ext)
+        shutil.move(path+'/'+files, path+'/'+ext+'/'+files)
